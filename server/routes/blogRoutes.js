52541eb4d24a33085d0e62a6eb007dbbe551b70c
M Omer Shakeel <omershakeel742@gmail.com>
2024-11-20 18:21:53 +0500

    API Creation/Testing Commit

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -3,4 +3,14 @@ const Blog = require("../models/Blog");
 const router = express.Router();
 
 //Get all blogs
-router.get("/", async (req,res) => {})
+router.get("/omer", (req,res) => {
+    try {
+        const blogs = Blog.find();
+        res.status(200).json(blogs);
+    }
+    catch (error){
+        res.status(500).json({
+            error: error.message
+        });
+    }
+});
